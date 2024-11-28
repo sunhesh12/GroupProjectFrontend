fdf6bc6b261260cc37ecf906c01d3765cd934278
sunhesh12 <hsdbandaranayake@gmail.com>
2024-11-28 23:00:17 +0530

    update greeting

--- src/app/(student)/dashboard/page.tsx
+++ src/app/(student)/dashboard/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import style from "./dashboard.module.css";
 
 export default function page() {
+  let User_Name = "Anura";
+
+  if (User_Name != null || User_Name == "") {
+    User_Name = User_Name;
+  } else {
+    User_Name = "Error Login";
+  }
   return (
     <>
-        
+      <div className={style.GreetingMessage}>
+        <h1>
+          Good Moring<span>{" " + User_Name}</span>
+        </h1>
+      </div>
     </>
-  )
+  );
 }
