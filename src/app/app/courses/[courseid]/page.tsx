c642409aff94bdf2c4802416967f201a6097b664
RasuwanK <kalharaweragala@gmail.com>
2025-02-08 01:51:20 +0530

    Moved to app folder

--- src/app/app/courses/[courseid]/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function CoursePage() {
-	return (
-		<>
-		</>
-	)
-}
--- src/app/app/courses/page.tsx
+++
@@ -1,9 +0,0 @@
-import Button from "@/components/button/view"
-export default function CoursesPage() {
-	return (
-		<>
-			<h1>All the courses</h1>
-			<Button isLink={true} href="/app/courses/create">Create a course</Button>
-		</>
-	)
-}
