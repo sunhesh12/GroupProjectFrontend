7998fb086a72d0518b4c51f8b716f18605727ef6
RasuwanK <kalharaweragala@gmail.com>
2025-02-24 23:52:14 +0530

    Moved schema

--- src/actions/signup.ts
+++ src/actions/signup.ts
@@ -1,7 +1,7 @@
 'use server';
 
-import createUser from '@/utils/create-user';
-import {z} from 'zod';
+import { user } from '@/utils/backend';
+import {signInSchema} from '@/utils/schema';
 
 export default async function signup(formData: FormData) {
 	const formValues = {
@@ -15,28 +15,13 @@ export default async function signup(formData: FormData) {
 		'course_id': formData.get('course_id')?.toString(),
 	};
 
-	const formSchema = z.object({
-		'email': z.string().email().nonempty(),
-		'full_name': z.string().nonempty(),
-		'age': z.number().int().min(16).max(100).nonnegative(),
-		'mobile_no': z.string().nonempty(),
-		'address': z.string().nonempty(),
-		'course_id': z.string().nonempty(),
-		'password': z.string().nonempty(),
-		'confirm_password': z.string().nonempty(),
-	}).refine(data => data.password === data.confirm_password, {
-		message: 'Passwords do not match',
-		path: ['confirm_password'],
-	});
-
 	// Checking for form validation error
-	const parsedForm = formSchema.safeParse(formValues);
-
+	const parsedForm = signInSchema.safeParse(formValues);
+	
 	if(!parsedForm.success) {
 		console.log(parsedForm.error);
 	} else {
-		formData.set('status', '1');
-		const result = await createUser(formData);	
+		const result = await user.auth.signup(formData);	
 		console.log(result);
 	}
 
