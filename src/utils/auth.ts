49e0ee08f6873b68008a864f8fec84944506c7b1
RasuwanK <kalharaweragala@gmail.com>
2025-02-24 23:52:54 +0530

    Implemented signup and signin

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,17 +1,77 @@
-import NextAuth from "next-auth"
-import Credentials from "next-auth/providers/credentials"
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { user } from "@/utils/backend";
+import { signInSchema } from "@/utils/schema";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-	Credentials({
-		credentials: {
-			email: {},
-			password: {}
-		},
-		authorize: async (credentials) => {
-			console.log(credentials);
-			return null;
-		}
-	})
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        let currentUser = null; 
+        const {email, password} = await signInSchema.parseAsync(credentials); 
+        
+        // Executing sign in route of backend
+        const response = await user.auth.signin({
+          email,
+          password
+        });
+
+        // Status of the response
+        if (response.success) {
+          // Successfull
+          if (response.payload?.user) {
+            // Sign in successfull
+            const { user, token } = response.payload;
+            currentUser = {
+              id: user.id,
+              name: user.Full_Name,
+              email: user.Email,
+              profilePicture: user.Profile_Picture,
+              accessToken: token,
+            };
+          } 
+        } else {
+          // Sign in failed
+          if (response.status == 401) {
+            // Invalid credentials
+            throw new Error("Error: Unmatching credentials !");
+          } else if(response.status == 500) {
+            // Server error
+            throw new Error("ServerError: Sign in request failed due to a server error !");
+          } else {
+            // Unknown error
+            console.log(response);
+            throw new Error("UnknownError: Sign in request failed !");
+          }
+        }
+
+        return currentUser;
+      },
+    }),
   ],
-})
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.user = user;
+      }
+
+      return token;
+    },
+    session: async ({ session, token }) => {
+      if (token) {
+        session.user = token.user;
+      }
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/auth/signin",
+  },
+});
