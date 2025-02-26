ee75bb4c31b1af407b72137bc0c8831652e42149
RasuwanK <kalharaweragala@gmail.com>
2025-02-26 19:59:33 +0530

    Improved way map form data into an object

--- src/actions/signup.ts
+++ src/actions/signup.ts
@@ -1,28 +1,23 @@
-'use server';
+"use server";
 
-import { user } from '@/utils/backend';
-import {signInSchema} from '@/utils/schema';
+import { user } from "@/utils/backend";
+import { signUpSchema } from "@/utils/schema";
 
 export default async function signup(formData: FormData) {
-	const formValues = {
-		'email': formData.get('email')?.toString(),
-		'full_name': formData.get('full_name')?.toString(),
-		'age': formData.get('age') !== null ? Number(formData.get('age')) : null,
-		'mobile_no': formData.get('mobile_no')?.toString(),
-		'address': formData.get('address')?.toString(),
-		'password': formData.get('password')?.toString(),
-		'confirm_password': formData.get('confirm_password')?.toString(),
-		'course_id': formData.get('course_id')?.toString(),
-	};
+  // Checking for form validation error
+  const parsedForm = await signUpSchema.safeParseAsync(
+    Object.fromEntries(
+      formData.entries().map(([key, value]) => {
+        if (key === "age") return [key, Number(value)];
+        else return [key, value];
+      })
+    )
+  );
 
-	// Checking for form validation error
-	const parsedForm = signInSchema.safeParse(formValues);
-	
-	if(!parsedForm.success) {
-		console.log(parsedForm.error);
-	} else {
-		const result = await user.auth.signup(formData);	
-		console.log(result);
-	}
-
+  if (!parsedForm.success) {
+    console.log(parsedForm.error);
+  } else {
+    const result = await user.auth.signup(formData);
+    console.log(result);
+  }
 }
