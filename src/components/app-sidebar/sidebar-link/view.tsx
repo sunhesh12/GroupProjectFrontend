281e071f479a75b7951758a6ecef2ba51d14572e
RasuwanK <kalharaweragala@gmail.com>
2025-03-01 14:54:13 +0530

    Sidebar links are adjusted when collapsed

--- src/components/app-sidebar/sidebar-link/view.tsx
+++ src/components/app-sidebar/sidebar-link/view.tsx
@@ -4,18 +4,52 @@ import Link from "next/link";
 import styles from "./style.module.css";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import { useState, useEffect, ReactNode } from "react";
+import { useState, useEffect, ReactNode, useMemo } from "react";
+import type { CSSProperties, MouseEventHandler } from "react";
 
-type SidebarLinkProps = {
+interface LinkIconProps {
   icon: string;
   alt: string;
-  href: string;
+  dimensions?: {
+    width: number;
+    height: number;
+  };
+  rounded?: boolean;
+}
+
+interface ComponentProps {
+  className?: string;
+  style: CSSProperties;
+  onClick?: MouseEventHandler;
+}
+
+function LinkIcon({ icon, alt, dimensions, rounded }: LinkIconProps) {
+  return (
+    <Image
+      alt={alt ?? "An icon"}
+      className={styles.linkIcon}
+      src={icon}
+      width={dimensions?.width ?? 15}
+      height={dimensions?.height ?? 15}
+      style={{
+        borderRadius: rounded ? "100%" : undefined,
+      }}
+    />
+  );
+}
+
+type SidebarLinkProps = {
+  icon?: string;
+  alt?: string;
+  href?: string;
   dimensions?: {
     width: number;
     height: number;
   };
   rounded?: boolean;
   children?: ReactNode;
+  onClick?: MouseEventHandler;
+  expanded: boolean;
 };
 
 export default function SidebarLink({
@@ -25,6 +59,8 @@ export default function SidebarLink({
   href,
   dimensions,
   rounded,
+  onClick,
+  expanded,
 }: SidebarLinkProps) {
   const pathname = usePathname();
   const [isActive, setActive] = useState(false);
@@ -36,27 +72,51 @@ export default function SidebarLink({
       setActive(false);
     }
   }, [pathname, href]);
+
+  // Common props for button and
+  const props = useMemo<ComponentProps>(() => {
+    return {
+      style: {
+        backgroundColor: isActive ? "#302B2B" : undefined,
+        justifyContent: expanded ? "left" : "center",
+        width: expanded ? "100%" : "40px",
+      },
+      onClick,
+      className: styles.link,
+    };
+  }, [isActive ,onClick, expanded]);
+
+  if (!href) {
+    return (
+      <button {...props}>
+        <div>
+          {icon && alt && (
+            <LinkIcon
+              icon={icon}
+              alt={alt}
+              dimensions={dimensions}
+              rounded={rounded}
+            />
+          )}
+        </div>
+        {expanded && <div className={styles.linkText}>{children}</div>}
+      </button>
+    );
+  }
+
   return (
-    <Link
-      className={styles.link}
-      style={isActive ? { backgroundColor: "#302B2B" } : undefined}
-      href={href}
-    >
+    <Link href={href} {...props}>
       <div>
-        <Image
-          alt={alt}
-          className={styles.linkIcon}
-          src={icon}
-          width={dimensions?.width ?? 15}
-          height={dimensions?.height ?? 15}
-          style={{
-            borderRadius: rounded ? "100%" : undefined,
-          }}
-        />
+        {icon && alt && (
+          <LinkIcon
+            icon={icon}
+            alt={alt}
+            dimensions={dimensions}
+            rounded={rounded}
+          />
+        )}
       </div>
-      <div className={styles.linkText}>
-        {children}
-      </div>
+      {expanded && <div className={styles.linkText}>{children}</div>}
     </Link>
   );
 }
